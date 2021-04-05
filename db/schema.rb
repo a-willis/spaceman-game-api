2b90442415b37ee59b35312598b95efca79cdcc2
Alexis Willis <alexiswillis97@gmail.com>
2021-04-05 17:10:13 -0400

    undo guesses table

--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_01_032931) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "games", force: :cascade do |t|
-    t.string "word"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "guesses", force: :cascade do |t|
-    t.string "letter_guessed"
-    t.integer "game_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  add_foreign_key "guesses", "games"
 end
