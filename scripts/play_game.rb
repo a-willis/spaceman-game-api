447f5b95ccefc040e8912e0a85366cadd8a6915b
Alexis Willis <alexiswillis97@gmail.com>
2021-05-25 16:27:47 -0400

    working (inefficient) server/client

--- scripts/play_game.rb
+++ scripts/play_game.rb
@@ -1,23 +1,63 @@
 require 'faraday'
 require 'ap'
+require 'json'
 
 def create_game_
   url = 'http://localhost:3000/games'
 
-  # POST JSON content
   response = Faraday.post(url, '{}',
   "Content-Type" => "application/json")
+  id = response.body[11..13].to_i
 
-  ap response.body
+  puts response.body
+  puts
+  puts response.body[11..13]
+  puts id
+  return id
 
 end
 
-url = 'http://localhost:3000/games/111'
+def calculate_next_guess
+  alphabet = "a".."z"
+  alphabet_arr = alphabet.to_a
+  alphabet_arr.sample
+end
 
-  # PUT JSON content
-response = Faraday.put(url, '{"guess": "u"}', "Content-Type" => "application/json")
+def make_a_guess(guess, id)
+  letter_guess = guess
+  puts "letter guess: #{letter_guess}"
+  a = '{"guess": "'+ "#{letter_guess}" + '"}'
 
-ap response.body
+  puts a
+  url = 'http://localhost:3000/games/' + "#{id}"
+  puts url
+  response = Faraday.put(url, a, "Content-Type" => "application/json")
+  return response.body
+  puts response.body
+end 
 
-#create_game_
+def random_guesses(id)
+  loop do
+    next_guess = calculate_next_guess()
+    puts "next guess: #{next_guess}"
 
+    resp = JSON.load(make_a_guess(next_guess, id))
+    puts "resp: #{resp}"
+
+    break if resp['game_over']
+  end 
+end 
+
+game_id = create_game_ 
+random_guesses(game_id)
+
+# create a game 
+# save id returned 
+# url = /games/#{id}
+# loop do
+# loop do
+#   next_guess = calculate_next_guess()
+#   resp = make a guess (next_guess)
+  
+#   break if resp.game_over
+# end
